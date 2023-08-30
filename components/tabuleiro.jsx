e56788464f98266d08155d6c0fac77f2107c4039
thiago henrique <1thiago1henrique@gmail.com>
2023-08-30 16:31:57 -0300

    finalização de tabuleiro em react

---
+++ components/tabuleiro.jsx
@@ -0,0 +1,16 @@
+import Linha from "./linha";
+
+export default function Tabuleiro() {
+    return (
+        <div>
+            <Linha/>
+            <Linha preta/>
+            <Linha/>
+            <Linha preta/>
+            <Linha/>
+            <Linha preta/>
+            <Linha/>
+            <Linha preta/>
+        </div>
+    )
+}
